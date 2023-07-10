28db44f9fa5b5a786b1f6386f3ec5c46b8f73601
zzoxi <zzoxi925@gmail.com>
2023-07-10 21:49:31 +0800

    7_10

---
+++ renren-generator/src/main/java/io/renren/dao/MySQLGeneratorDao.java
@@ -0,0 +1,18 @@
+package io.renren.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ * MySQL代码生成器
+ *
+ * @author Mark sunlightcs@gmail.com
+ * @since 2018-07-24
+ */
+@Mapper
+public interface MySQLGeneratorDao extends GeneratorDao {
+
+
+}
